33faa50886038d2ee31ceebce16d19aa30e418b0
jay_nagaria <nagariajay511@gmail.com>
2023-06-29 23:54:12 -0400

    Home menu updates

---
+++ Task_Master/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Task_Master
+//
+//  Created by J on 6/29/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
